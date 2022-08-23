f44338582c609ffe1b3ec6f5c6736051b727055f
msmaxiii <maxwellrg@gmail.com>
2022-08-22 23:22:24 -0500

    employees completed

---
+++ employee.sql
@@ -0,0 +1,9 @@
+1. SELECT first_name, last_name FROM employee WHERE city = 'Calgary';
+
+2.SELECT MAX(birth_date) from employee;
+
+3.SELECT MIN(birth_date) from employee;
+
+4.SELECT * FROM employee WHERE reports_to = 2;
+
+5.SELECT COUNT (*) employee WHERE city = 'Lethbridge';
