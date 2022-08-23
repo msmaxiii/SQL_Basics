18d9863e927d4e9033390ba871d6b371269dd331
msmaxiii <maxwellrg@gmail.com>
2022-08-22 23:44:23 -0500

    done

--- invoice.sql
+++ invoice.sql
@@ -1,1 +1,23 @@
-1.
+1.SELECT COUNT (*) FROM invoice WHERE billing_country ='USA';
+
+2.SELECT MAX (total) FROM invoice;
+
+3.SELECT MIN (total) FROM invoice;
+
+4.SELECT *  FROM invoice WHERE (total) >5;
+
+5.SELECT COUNT(*) FROM invoice WHERE (total) <5;
+
+6. SELECT* FROM invoice WHERE billing_city IN('CA','AZ','TX');
+
+7.SELECT AVG (total) FROM invoice;
+
+8. SELECT SUM (total) FROM invoice;
+
+9.UPDATE invoice
+SET total = 24
+WHERE invoice_id = 5;
+
+10.DELETE 
+FROM INVOICE
+WHERE invoice_id = 1;
